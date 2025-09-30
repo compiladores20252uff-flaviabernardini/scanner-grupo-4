71829d1e8a7f183d50a653acd2a7ac13c1b140c0
CledsonSilvaAraujo <ciebinho.embetant@gmail.com>
2025-09-29 21:00:18 -0300

    Refatorar lexer para melhorar tratamento de erros e formatação de tokens

--- calculadora/Token.java
+++ calculadora/Token.java
@@ -3,16 +3,33 @@ public class Token {
   public final String lexeme;
   public final int line;
   public final int column;
+  public final String message;
 
   public Token(TokenType type, String lexeme, int line, int column) {
-      this.type = type;
-      this.lexeme = lexeme;
-      this.line = line;
-      this.column = column;
+    this(type, lexeme, line, column, null);
   }
+
+  public Token(TokenType type, String lexeme, int line, int column, String message) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.line = line;
+    this.column = column;
+    this.message = message;
+  }
+
+  public boolean isError() { return type == TokenType.ERROR; }
 
   @Override
   public String toString() {
-      return String.format("O token { tipo=%s, lexema='%s', linha=%d, coluna=%d }", type, lexeme, line, column);
+    if (isError()) {
+      return String.format(
+        "Erro lexico { lexema='%s', linha=%d, coluna=%d%s }",
+        lexeme, line, column, message != null ? ", mensagem='" + message + "'" : ""
+      );
+    }
+    return String.format(
+      "O token { tipo=%s, lexema='%s', linha=%d, coluna=%d }",
+      type, lexeme, line, column
+    );
   }
 }
--- calculadora/TokenType.java
+++ calculadora/TokenType.java
@@ -2,5 +2,6 @@ public enum TokenType {
   LPAREN, RPAREN,
   PLUS, MINUS, STAR, SLASH, INT_DIV, POW,
   INT, FLOAT,
-  EOF
+  EOF,
+  ERROR
 }
